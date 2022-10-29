491ce8c9b9ee7e223274904437cb2caa6703ad80
Sebastian Romanczuk <sebastian.romanczuk1999@gmail.com>
2022-10-29 22:36:39 +0200

    files added final

--- javascript.js
+++ javascript.js
@@ -1,12 +1,19 @@
 console.log("HELLO KANTOR NIE BEMA")
-let formElement = document.querySelector(".js-F");
-let amountElement = document.querySelector(".js-A");
-let resultElement = document.querySelector(".js-R");
-let currencyElement = document.querySelector(".js-WC");
+
+
+const formElement = document.querySelector(".js-F");
+
+const amountElement = document.querySelector(".js-A");
+
+const resultElement = document.querySelector(".js-R");
+
+const currencyElement = document.querySelector(".js-WC");
+
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
     let amount = amountElement.value;
     let currency = currencyElement.value;
     let result = (amount / currency);
     resultElement.innerText = result.toFixed(2);
-}); 
+});
+
